445c0f7900d7d1f352e7ac1dcd6050afd91bbc62
Susan Tan <ssusantan9@gmail.com>
2019-03-25 04:44:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,9 @@ def roll_call_dwarves(arr)
   end
 end 
   
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(arr)
+  newArr = arr.collect |x|
+  x
 end
 
 def long_planeteer_calls# code an argument here
